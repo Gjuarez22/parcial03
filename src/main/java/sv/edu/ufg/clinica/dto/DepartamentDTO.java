a8845d019204bee70aafe05b730e89b2ce071406
UFG <emilioderas3@gmail.com>
2023-11-04 11:59:45 -0600

    |dept

--- src/main/java/sv/edu/ufg/clinica/dto/DepartamentDTO.java
+++ src/main/java/sv/edu/ufg/clinica/dto/DepartamentDTO.java
@@ -1,4 +1,15 @@
 package sv.edu.ufg.clinica.dto;
 
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class DepartamentDTO {
+
+    private Integer idDepartament;
+
+    @NotNull
+    @Size(min = 3, message = "{name.size}")
+    private String name;
 }
